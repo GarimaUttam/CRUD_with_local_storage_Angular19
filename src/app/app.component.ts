0d3ce28e417b440854fd69b716d9a74766abc9c6
GarimaUttam <garimauttam191203@gmail.com>
2025-01-24 10:27:45 +0530

    basic CRUD done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular_19_local_storage_crud';
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    debugger;
+    const localData = localStorage.getItem('angular19User');
+    if(localData != null){
+      this.userList = JSON.parse(localData);
+    }
+  }
+
+  isNewUser: boolean = false;
+
+  userObj: User = new User();
+  userList: User[] = [];
+
+  changeView() {
+    this.isNewUser = !this.isNewUser;
+  }
+
+  onSave() {
+    debugger;
+    this.userObj.userId = this.userList.length + 1;
+    this.userList.push(this.userObj);
+    this.userObj = new User();
+    localStorage.setItem('angular19User', JSON.stringify(this.userList));
+    this.changeView();
+  }
+
+  onEdit(data: User){
+    this.userObj = data;
+    this.changeView();
+  }
+  onUpdate(){
+    const record = this.userList.find(m=>m.userId == this.userObj.userId);
+    if(record != undefined){
+      record.fName = this.userObj.fName;
+      record.lName = this.userObj.lName;
+      record.uName = this.userObj.uName;
+      record.city = this.userObj.city;
+      record.state = this.userObj.state;
+      record.zipCode = this.userObj.zipCode;
+      record.isAgree = this.userObj.isAgree;
+    }
+    localStorage.setItem('angular19User', JSON.stringify(this.userList));
+    this.changeView();
+  }
+  onDelete(userId: number){
+    const isDelete = confirm("Are yoy=u sure to delete this card ?");
+    if(isDelete){
+      const index = this.userList.findIndex(m => m.userId == userId);
+      this.userList.splice(index, 1);
+      localStorage.setItem('angular19User', JSON.stringify(this.userList));
+    }
+
+  }
+}
+
+
+class User {
+  userId: number;
+  fName: string;
+  lName: string;
+  uName: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  isAgree: boolean;
+
+  constructor(){
+    this.userId = 0;
+    this.fName = "";
+    this.lName = "";
+    this.uName = "";
+    this.city = "";
+    this.state = "";
+    this.zipCode = "";
+    this.isAgree = false;
+  }
 }
